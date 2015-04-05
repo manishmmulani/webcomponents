d2c1611f0a7fb7e742a6f27caea3c6ea2aee3b1b
Manish Mulani <mulanimonty@gmail.com>
2015-04-05 22:25:25 +0530

    Adding Polymer applications 1. Google Map 2. VCard 3. Interest Calculator

---
+++ PolymerMapsCodeLab/PolyerMapsCodeLab/index.html.0.js
@@ -0,0 +1,13 @@
+
+    // function searchMap() {
+    //   var gMapSearch = document.querySelector('google-map-search');
+    //   gMapSearch.search();
+    //   console.log(gMapSearch.result);
+    // }
+    // When template is="auto-binding" is not there, use the following    
+    // var gMap = document.querySelector('google-map');
+    // gMap.addEventListener('api-load', function(e) {
+    //   document.querySelector('google-map-directions').map = this.map;
+    
+    // });
+  
